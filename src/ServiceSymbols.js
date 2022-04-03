db41f2d96ed95efa4c2a4163992e887cbf42e821
tvz <tvz.041@gmail.com>
2022-04-03 20:54:26 +0700

    Изменён порядок обработки служебных символов

--- src/ServiceSymbols.js
+++ src/ServiceSymbols.js
@@ -37,36 +37,36 @@ const ServiceSymbols = {
 
     init: function() {
         Array.prototype.push.apply(this.All, this.Parenthesis);
-        Array.prototype.push.apply(this.All, this.OneSymbolOperators);
         Array.prototype.push.apply(this.All, this.TwoSymbolOperators);
-        Array.prototype.push.apply(this.All, this.SingleQuotes);
+        Array.prototype.push.apply(this.All, this.OneSymbolOperators);
         Array.prototype.push.apply(this.All, this.DoubleQuotes);
+        Array.prototype.push.apply(this.All, this.SingleQuotes);
         Array.prototype.push.apply(this.All, this.Whitespaces);
         Array.prototype.push.apply(this.All, this.TexSpecialSymbols);
 
-        Array.prototype.push.apply(this.AllExceptParenthesis, this.OneSymbolOperators);
         Array.prototype.push.apply(this.AllExceptParenthesis, this.TwoSymbolOperators);
-        Array.prototype.push.apply(this.AllExceptParenthesis, this.SingleQuotes);
+        Array.prototype.push.apply(this.AllExceptParenthesis, this.OneSymbolOperators);
         Array.prototype.push.apply(this.AllExceptParenthesis, this.DoubleQuotes);
+        Array.prototype.push.apply(this.AllExceptParenthesis, this.SingleQuotes);
         Array.prototype.push.apply(this.AllExceptParenthesis, this.Whitespaces);
         Array.prototype.push.apply(this.AllExceptParenthesis, this.TexSpecialSymbols);
 
         Array.prototype.push.apply(this.AllExceptQuotes, this.Parenthesis);
-        Array.prototype.push.apply(this.AllExceptQuotes, this.OneSymbolOperators);
         Array.prototype.push.apply(this.AllExceptQuotes, this.TwoSymbolOperators);
+        Array.prototype.push.apply(this.AllExceptQuotes, this.OneSymbolOperators);
         Array.prototype.push.apply(this.AllExceptQuotes, this.Whitespaces);
         Array.prototype.push.apply(this.AllExceptQuotes, this.TexSpecialSymbols);
 
         Array.prototype.push.apply(this.AllExceptWhitespaces, this.Parenthesis);
-        Array.prototype.push.apply(this.AllExceptWhitespaces, this.OneSymbolOperators);
         Array.prototype.push.apply(this.AllExceptWhitespaces, this.TwoSymbolOperators);
-        Array.prototype.push.apply(this.AllExceptWhitespaces, this.SingleQuotes);
+        Array.prototype.push.apply(this.AllExceptWhitespaces, this.OneSymbolOperators);
         Array.prototype.push.apply(this.AllExceptWhitespaces, this.DoubleQuotes);
+        Array.prototype.push.apply(this.AllExceptWhitespaces, this.SingleQuotes);
         Array.prototype.push.apply(this.AllExceptWhitespaces, this.TexSpecialSymbols);
 
         Array.prototype.push.apply(this.AllExceptWhitespacesAndSingleQuotes, this.Parenthesis);
-        Array.prototype.push.apply(this.AllExceptWhitespacesAndSingleQuotes, this.OneSymbolOperators);
         Array.prototype.push.apply(this.AllExceptWhitespacesAndSingleQuotes, this.TwoSymbolOperators);
+        Array.prototype.push.apply(this.AllExceptWhitespacesAndSingleQuotes, this.OneSymbolOperators);
         Array.prototype.push.apply(this.AllExceptWhitespacesAndSingleQuotes, this.DoubleQuotes);
         Array.prototype.push.apply(this.AllExceptWhitespacesAndSingleQuotes, this.TexSpecialSymbols);
 
